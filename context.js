ea660d79f0269c9cf1fe46512cf885c1c530a12c
Omega Dziko F. <megaoy@yahoo.com>
2024-04-29 20:39:51 -0400

    answered

--- context.js
+++ context.js
@@ -15,23 +15,23 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+//eat(); // undefined because it is refferencing the global obj
 
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
-// nemo.eat(); // ?
+// nemo.eat(); // prints 'Nemo eats fish food' because eat fun assgn to nemo obj then invoked
 
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
-// eat(); // ?
+// eat(); // undefined eats fish food because we have yet to bind the func to an obj other than global
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+//nemo.swim(); // nemo swimming in the water
 
 
 /********************************* Scenario 5 *********************************/
 // const swim = nemo.swim;
-// swim(); // ?
+// swim(); // error
